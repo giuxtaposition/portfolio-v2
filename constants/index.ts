ccb0c77d406116a1debc9b67bd905d3d59f2df5b
giuxtaposition <yg97.cs@gmail.com>
2021-06-17 17:07:58 +0200

    Projects Page

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,6 @@
+import { AiTwotoneThunderbolt } from 'react-icons/ai'
+import { FaStore, FaDatabase } from 'react-icons/fa'
+import { BsGearFill } from 'react-icons/bs'
 import { FiMail, FiHome, FiUser } from 'react-icons/fi'
 import { FaLaptopCode } from 'react-icons/fa'
 import {
@@ -12,6 +15,10 @@ import {
   SiVisualstudiocode,
   SiSass,
 } from 'react-icons/si'
+
+import sketchpad from '../public/images/projects/sketchpad.png'
+import todo from '../public/images/projects/todo.png'
+import portfolio_v1 from '../public/images/projects/portfolio-v1.png'
 
 export const NavLinks = [
   {
@@ -86,5 +93,72 @@ export const techStack = [
     name: 'Git',
     icon: SiGit,
     color: 'orange.500',
+  },
+]
+
+export const projectsFilters = [
+  {
+    name: 'All',
+    icon: AiTwotoneThunderbolt,
+  },
+  {
+    name: 'Frontend',
+    icon: FaStore,
+  },
+  {
+    name: 'Backend',
+    icon: FaDatabase,
+  },
+  {
+    name: 'Other',
+    icon: BsGearFill,
+  },
+]
+
+export const projects = [
+  {
+    title: 'Sketchpad',
+    img: sketchpad,
+    description: 'A simple site to make some pixel art',
+    projectLink: 'https://giuxtaposition.github.io/sketchpad/',
+    projectGithub: 'https://github.com/giuxtaposition/sketchpad',
+    id: 'sketchpad',
+    category: ['Frontend'],
+    details: {
+      language: 'Javascript',
+      framework: '',
+      library: 'React',
+      database: '',
+    },
+  },
+  {
+    title: 'Todo App',
+    img: todo,
+    description: 'A Todo List App',
+    projectLink: 'https://giuxtaposition.github.io/todo-app/',
+    projectGithub: 'https://github.com/giuxtaposition/todo-app',
+    id: 'todo',
+    category: ['Frontend', 'Backend'],
+    details: {
+      language: 'Javascript',
+      framework: '',
+      library: 'React, Material-UI',
+      database: 'Firebase Firestore Database',
+    },
+  },
+  {
+    title: 'Portfolio Site',
+    img: portfolio_v1,
+    description: 'Portofolio Site V1',
+    projectLink: 'https://giuxtaposition.github.io/portfolio-v1',
+    projectGithub: 'https://github.com/giuxtaposition/portfolio-v1',
+    id: 'portofolio-v1',
+    category: ['Frontend', 'Backend'],
+    details: {
+      language: 'Typescript, Sass',
+      framework: 'Express.js',
+      library: 'React, Chakra UI, Node.js',
+      database: '',
+    },
   },
 ]
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,12 +1,89 @@
 import Head from 'next/head'
+import React from 'react'
+import {
+  Stack,
+  Box,
+  useColorModeValue,
+  Heading,
+  HStack,
+  Tabs,
+  TabPanel,
+  TabPanels,
+} from '@chakra-ui/react'
+import ProjectsFilters from '../components/Projects/ProjectsFilters'
+import ProjectsPanel from '../components/Projects/ProjectsPanel'
+import { projectsFilters, projects } from '../constants'
+import { project } from '../components/Projects/types'
 
 export default function Projects() {
+  const handleTabFilter = (project: project, tab: string) => {
+    if (tab === 'All') {
+      return project
+    }
+
+    for (const category of project.category) {
+      if (category === tab) {
+        return project
+      }
+    }
+  }
+
   return (
     <>
       <Head>
         <title>Projects</title>
       </Head>
-      <section>Projects</section>
+      <Stack>
+        {/*Title*/}
+        <Heading
+          mt={10}
+          mb={6}
+          fontSize='3xl'
+          lineHeight='shorter'
+          fontWeight='bold'
+          textAlign='center'
+        >
+          <Box as='span' display='inline-block' position='relative'>
+            Projects
+            <Box
+              as='span'
+              display='block'
+              position='absolute'
+              bg={useColorModeValue('gray.800', 'gray.200')}
+              w='100%'
+              h='2px'
+              bottom={-2}
+            />
+          </Box>
+        </Heading>
+
+        <HStack>
+          {/*Filters*/}
+          <Tabs
+            variant='soft-rounded'
+            colorScheme='blue'
+            align='center'
+            w='100%'
+            defaultIndex={0}
+            isLazy
+          >
+            <ProjectsFilters />
+
+            {/*Projects*/}
+            <TabPanels minHeight={'45vh'}>
+              {projectsFilters.map((tab, index) => (
+                <TabPanel py={4} key={index}>
+                  <ProjectsPanel
+                    projects={projects.filter(project =>
+                      handleTabFilter(project, tab.name)
+                    )}
+                  />
+                </TabPanel>
+              ))}
+            </TabPanels>
+          </Tabs>
+        </HStack>
+      </Stack>
     </>
   )
 }
