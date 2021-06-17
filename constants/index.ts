5e2b80577a87d30b41a6d5d9af286820a9574b71
giuxtaposition <yg97.cs@gmail.com>
2021-06-17 16:23:50 +0200

    About Page

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,17 @@
 import { FiMail, FiHome, FiUser } from 'react-icons/fi'
 import { FaLaptopCode } from 'react-icons/fa'
+import {
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiTypescript,
+  SiReact,
+  SiNodeDotJs,
+  SiFirebase,
+  SiGit,
+  SiVisualstudiocode,
+  SiSass,
+} from 'react-icons/si'
 
 export const NavLinks = [
   {
@@ -21,5 +33,58 @@ export const NavLinks = [
     name: 'Contact',
     path: '/contact',
     icon: FiMail,
+  },
+]
+
+export const techStack = [
+  {
+    name: 'HTML5',
+    icon: SiHtml5,
+    color: 'orange.500',
+  },
+  {
+    name: 'CSS',
+    icon: SiCss3,
+    color: 'blue.500',
+  },
+  {
+    name: 'Javascript',
+    icon: SiJavascript,
+    color: 'yellow.400',
+  },
+  {
+    name: 'Typescript',
+    icon: SiTypescript,
+    color: 'blue.400',
+  },
+  {
+    name: 'React',
+    icon: SiReact,
+    color: 'cyan.400',
+  },
+  {
+    name: 'NodeJs',
+    icon: SiNodeDotJs,
+    color: 'green.400',
+  },
+  {
+    name: 'Firebase',
+    icon: SiFirebase,
+    color: 'yellow.500',
+  },
+  {
+    name: 'Visual Studio Code',
+    icon: SiVisualstudiocode,
+    color: 'blue.500',
+  },
+  {
+    name: 'Sass',
+    icon: SiSass,
+    color: 'pink.500',
+  },
+  {
+    name: 'Git',
+    icon: SiGit,
+    color: 'orange.500',
   },
 ]
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head'
+import React from 'react'
+import { VStack, Box, Text, Heading, useColorModeValue } from '@chakra-ui/react'
+import TechStack from '../components/TechStack'
 
 export default function About() {
   return (
@@ -6,7 +9,42 @@ export default function About() {
       <Head>
         <title>About</title>
       </Head>
-      <section>About</section>
+      <VStack
+        justifyContent='center'
+        alignItems='center'
+        spacing={2}
+        h='calc(100vh - 6em)'
+      >
+        {/* Intro */}
+        <VStack mb={6}>
+          <Heading p={1} color={useColorModeValue('purple.600', 'purple.300')}>
+            Let Me Introduce Myself
+          </Heading>
+          <Text>
+            Hi! I&apos;m Giulia and I like building things that live on the
+            internet.
+          </Text>
+          <Text>My field of Interest are Web and Mobile Development.</Text>
+          <Text>
+            Strong Believer of the Open-source-software movement! Let&apos;s
+            make something amazing!
+          </Text>
+        </VStack>
+
+        {/*Tech Stack*/}
+        <VStack>
+          <Heading color={useColorModeValue('purple.600', 'purple.300')}>
+            Tech Stack
+          </Heading>
+          <Text py={2}>
+            A list of my favorite tools and technologies that I use on a regular
+            basis.
+          </Text>
+          <Box>
+            <TechStack />
+          </Box>
+        </VStack>
+      </VStack>
     </>
   )
 }
