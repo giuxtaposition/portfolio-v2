46dd763665201baaf08e05315b48678ffa0e1bcd
giuxtaposition <yg97.cs@gmail.com>
2021-06-18 11:38:10 +0200

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 const debug = process.env.NODE_ENV !== 'production'
 
-// next.js configuration
-const nextConfig = {
+module.exports = {
   assetPrefix: !debug ? '/portfolio-v2/' : '',
 }
-
-module.exports = withPlugins([], nextConfig)
