052aee3412be9188d587492c31c943e0fe3fa8d6
giuxtaposition <yg97.cs@gmail.com>
2021-06-18 12:43:53 +0200

    removed files for github pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const debug = process.env.NODE_ENV !== 'production'
 const path = require('path')
 
 module.exports = {
-  assetPrefix: !debug ? '/portfolio-v2/' : '',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
