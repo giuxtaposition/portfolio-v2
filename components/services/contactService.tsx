d4cccf169f114464e557a6e234625f4fe7b9a1da
giuxtaposition <yg97.cs@gmail.com>
2021-07-01 18:30:32 +0200

    fixed contact service

--- components/services/contactService.tsx
+++ components/services/contactService.tsx
@@ -1,9 +1,9 @@
-import axios from "axios";
-const baseUrl = "https://giuxtaposition.tech/portfolio/contact";
+import axios from 'axios'
+const baseUrl = 'https://api.giuxtaposition.tech/contact'
 
 const sendMail = (newObject: any) => {
-  const request = axios.post(baseUrl, newObject);
-  return request.then((response) => response.data);
-};
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
 
-export default sendMail;
+export default sendMail
